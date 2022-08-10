2684fe3c03d9879c4c4e91ec8e0da67b922c7668
eeea-cc <59748709+eeea-cc@users.noreply.github.com>
2022-08-11 03:20:01 +0800

    Create uninstall_aliyun-service.sh

---
+++ uninstall_aliyun-service.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+pkill aegis
+systemctl stop aegis
+systemctl disable aegis
+
+d='/tmp/uninstall_aliyun-service/'
+mkdir $d
+cd $d
+curl -o uninstall.sh "http://update.aegis.aliyun.com/download/uninstall.sh"
+bash uninstall.sh
+rm -rf uninstall.sh
+
+curl -o uninstall.sh "http://update.aegis.aliyun.com/download/quartz_uninstall.sh"
+bash uninstall.sh
+rm -rf uninstall.share
+
+rm -rf /etc/systemd/system/aegis.service
+rm -rf /etc/rc2.d/S80aegis
+rm -rf /etc/rc3.d/S80aegis
+rm -rf /etc/rc4.d/S80aegis
+rm -rf /etc/rc5.d/S80aegis
+rm -rf /etc/rc.d/rc2.d/S80aegis
+rm -rf /etc/rc.d/rc3.d/S80aegis
+rm -rf /etc/rc.d/rc4.d/S80aegis
+rm -rf /etc/rc.d/rc5.d/S80aegis
+rm -rf /usr/local/aegis
+
+
+pkill aliyun-service
+/usr/local/share/assist-daemon/assist_daemon --stop
+/usr/local/share/assist-daemon/assist_daemon --delete
+rm -rf /usr/sbin/aliyun*
+rm -rf /usr/local/share/aliyun-assist
+rm -rf /usr/local/share/assist-daemon
+rm -rf /tmp/aliyun_assist_*
+
+rm -rf $d
